b82aa7c6aafb7c198410cfdabe966ab11a929a00
ahsan <makabir98@yahoo.com>
2023-04-07 13:37:16 +0600

    navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Container } from 'react-bootstrap'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Store } from './pages/Store'
+import { About } from './pages/About'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
-    <h1>Hi</h1>
+    <>
+      <Navbar />
+      <Container className='mb-4'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+    </>
   )
 }
 
